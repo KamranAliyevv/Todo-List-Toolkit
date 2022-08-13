88ca961ba329219ef5cd740fe5fa38fc0a463262
Kamran <kamraneliyev002@gmail.com>
2022-08-13 18:13:08 +0400

    add toastify and hover

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,12 +3,18 @@ import {BsPlus} from "react-icons/bs";
 import { useDispatch, useSelector } from 'react-redux';
 import { editsave } from '../../redux/reducer/todoEditReducer';
 import { add, edit } from '../../redux/reducer/todoReducer';
-
+import { ToastContainer,toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Form = () => {
   const inputRef = useRef();
   const dispatch=useDispatch();
   const {editData}=useSelector(store=>store);
+  const notify = (text,type) => toast(text,{
+    position:"top-right",
+    autoClose:5000,
+    type:type
+  });
 
   function addItem(e){
     e.preventDefault();
@@ -21,6 +27,7 @@ const Form = () => {
       dispatch(editsave({
         id:null
       }))
+      notify("Item edited","success");
     }
     else{
       dispatch(add({
@@ -28,7 +35,12 @@ const Form = () => {
         text:inputValue,
         checked:false,
       }))
+
+      notify("Item added","success");
     }
+    }
+    else{
+      notify("Input cannot be empty","error");
     }
     inputRef.current.value="";
   }
@@ -39,10 +51,13 @@ const Form = () => {
     }
   },[editData])
   return (
+    <>
     <form onSubmit={addItem} id='todo-form'>
         <input ref={inputRef} type="text" placeholder='Tapşırığı daxil edin' />
         <button className='addTodo'><BsPlus/></button>
     </form>
+    <ToastContainer/>
+    </>
   )
 }
 
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,62 +1,94 @@
-import React from 'react'
-import {BsCheck} from "react-icons/bs";
-import {RiDeleteBinLine} from "react-icons/ri";
-import {MdEdit} from "react-icons/md";
+import React from "react";
+import { BsCheck } from "react-icons/bs";
+import { RiDeleteBinLine } from "react-icons/ri";
+import { MdEdit } from "react-icons/md";
 import emptyPng from "../../assets/images/empty.png";
-import { useDispatch, useSelector } from 'react-redux';
-import { remove, removeAll, update } from '../../redux/reducer/todoReducer';
-import { editsave } from '../../redux/reducer/todoEditReducer';
+import { useDispatch, useSelector } from "react-redux";
+import { remove, removeAll, update } from "../../redux/reducer/todoReducer";
+import { editsave } from "../../redux/reducer/todoEditReducer";
+import {toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const TodoList = () => {
-    const {todo}=useSelector(store=>store);
-    const dispatch=useDispatch();
-    function removeItem(id){
-        dispatch(remove(id));
-        dispatch(editsave({id:null}))
-    }
-    function updateItem(id){
-        dispatch(update(id))
-    }
-    function removeAllItem(id){
-        dispatch(removeAll())
-        dispatch(editsave({id:null}))
-    }
-    function editItem(e,item){
-        e.stopPropagation();
-        dispatch(editsave(item))
-    }
+  const notify = (text) => toast(text,{
+    position:"top-right",
+    autoClose:5000,
+    type:"success"
+  });
+  const { todo } = useSelector((store) => store);
+  const dispatch = useDispatch();
+  function removeItem(e,id) {
+    e.stopPropagation();
+    dispatch(remove(id));
+    dispatch(editsave({ id: null }));
+    notify("Item deleted");
+  }
+  function updateItem(id) {
+    dispatch(update(id));
+    notify("Item updated");
+  }
+  function removeAllItem(id) {
+    dispatch(removeAll());
+    dispatch(editsave({ id: null }));
+    notify("All item deleted");
+  }
+  function editItem(e, item) {
+    e.stopPropagation();
+    dispatch(editsave(item));
+  }
   return (
-    <div className='todo-list'>
-        {todo?.length>0 ?
+    <div className="todo-list">
+      {todo?.length > 0 ? (
         <ul>
-            {todo.map(item=>{
-            return(
-            <li onClick={()=>updateItem(item.id)} key={item.id} className={item.checked ? "checked" : null}>
-                <span className="checkbox"><BsCheck/></span>
+          {todo.map((item) => {
+            return (
+              <li
+                onClick={() => updateItem(item.id)}
+                key={item.id}
+                className={item.checked ? "checked" : null}
+              >
+                <span className="checkbox">
+                  <BsCheck />
+                </span>
                 <p>{item.text}</p>
                 <div className="todo-btns">
-                    <div className="edit-btn" onClick={(e)=>editItem(e,item)}><MdEdit/></div>
-                    <div className="delete-btn" onClick={()=>removeItem(item.id)}><RiDeleteBinLine/></div>
+                  <div className="edit-btn" onClick={(e) => editItem(e, item)}>
+                    <MdEdit />
+                  </div>
+                  <div
+                    className="delete-btn"
+                    onClick={(e) => removeItem(e,item.id)}
+                  >
+                    <RiDeleteBinLine />
+                  </div>
                 </div>
-            </li>
-            )
-            })}
+              </li>
+            );
+          })}
         </ul>
-        :
+      ) : (
         <div className="empty-list">
-            <img src={emptyPng} alt="" />
-            <span>Heç bir tapşırıq yoxdur</span>
+          <img src={emptyPng} alt="" />
+          <span>Heç bir tapşırıq yoxdur</span>
         </div>
-}
-            <div className="todo-footer">
-                <span>Ümumi: {todo?.length} tapşırıq</span>
-                <span>Hazır: {todo.filter(item=>{
-                    if(item.checked)return item;
-                    return false
-                }).length} tapşırıq</span>
-                <span onClick={removeAllItem} className="clear-all">Hamısını sil</span>
-            </div>
+      )}
+      <div className="todo-footer">
+        <span>Ümumi: {todo?.length} tapşırıq</span>
+        <span>
+          Hazır:{" "}
+          {
+            todo.filter((item) => {
+              if (item.checked) return item;
+              return false;
+            }).length
+          }{" "}
+          tapşırıq
+        </span>
+        <span onClick={removeAllItem} className="clear-all">
+          Hamısını sil
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TodoList
+export default TodoList;
